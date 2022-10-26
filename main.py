fe1c0cb0a8b513fb94d35069b9d3a952038ec7ff
Theo <theosmithscott@protonmail.com>
2022-10-27 00:02:05 +0100

    Update: - Added signup page template - Added signup page url routing - Changed navbar button text to relevant directories

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask, url_for, redirect, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html", title="Home Page")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html", title="Create an Account")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
