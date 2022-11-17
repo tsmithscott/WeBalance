70b80ff76eeef37602ad4ea9c244c1595d7d1a43
Theo <theosmithscott@protonmail.com>
2022-11-17 12:35:52 +0000

    Added templates and routing

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", title="Home Page")
+    return redirect(url_for("about"))
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title="Login")
 
 
 @app.route("/signup")
 def signup():
     return render_template("signup.html", title="Create an Account")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html", title="Dashboard")
+
+
+@app.route("/preferences")
+def preferences():
+    return render_template("preferences.html", title="Preferences")
+
+
+@app.route("/about")
+def about():
+    return render_template("index.html", title="About Us")
 
 
 if __name__ == '__main__':
